693f75090843f19017194ed06bbd9b421402d73f
MH4GF <h.miyagi.cnw@gmail.com>
2022-09-24 20:32:22 +0900

    setup jest

---
+++ apps/server/src/modules/moneyforward/repository/__tests__/getBalance.test.ts
@@ -0,0 +1,11 @@
+import { getBalance } from "../getBalance";
+
+describe("getBalance", () => {
+  it("should return balance", () => {
+    expect(getBalance()).toEqual({
+      id: "xxx",
+      income: 100,
+      expense: 100,
+    });
+  });
+});
