b3082c5d57876467e6ca9b34b5b823d424a227cf
MH4GF <h.miyagi.cnw@gmail.com>
2022-09-24 16:52:21 +0900

    setup test server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
